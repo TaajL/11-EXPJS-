5a0c9c4847628859e29434d8355af87a662c53a1
Malcom X <Taajlogan@gmail.com>
2024-05-08 13:25:54 -0400

    addedGETroutetoretrieve all notesfromDB

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const { json } = require('body-parser');
 const app = express();
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(json.parse(data));
+    });
+});
